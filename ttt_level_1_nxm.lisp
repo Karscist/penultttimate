90ce51d11c45e5d2fcfe5643d6569550b84ec9c8
hexa6 <hexathe6@gmail.com>
2025-06-22 11:55:55 -0500

    added board win-checking and fixed issue with format string

--- ttt_level_1_nxm.lisp
+++ ttt_level_1_nxm.lisp
@@ -3,19 +3,36 @@
 
 (setq *print-circle* t)
 
-(defpackage "penultttimate-level-1" (:use "COMMON-LISP") (:shadow "DEREF"))
+(defpackage "penultttimate-level-1"
+  (:use "COMMON-LISP")
+  (:shadow "DEREF" "LOOP"))
 (in-package "penultttimate-level-1")
 
 ;; this will be partially written in a pipettish style because
 ;; i forgot how to write decent CL (and don't want to)
 
-(defmacro f (function) (list 'funcall function))
+(defmacro f (function &rest rest) `(funcall ,function ,@rest))
 
+(defun id (x) x)
 (defun const (x) (lambda (_) (declare (ignore _)) x))
 (defun duplist (l) (mapcar (lambda (x) (if (typep x 'list) (duplist x) x)) l))
 
 (defun calltimes (n f)
   (when (> n 0) (cons (funcall f n) (calltimes (- n 1) f))))
+
+;; there is definitely a more efficient method to do this
+;; but this is easy to write
+(defun range (min max)
+  (mapcar (lambda (x) (- x (+ 1 min))) (nreverse (calltimes (- max min) #'id))))
+
+(defun split-list (l)
+  (cons
+   (remove-if
+    #'null
+    (mapcar (lambda (x n) (when (evenp n) x)) l (range 0 (length l))))
+   (remove-if
+    #'null
+    (mapcar (lambda (x n) (when (oddp n) x)) l (range 0 (length l))))))
 
 (defmacro match (val eq def &body body)
   `(cond
@@ -28,6 +45,16 @@
 (defmacro let-1 (sym val &body body) `(let ((,sym ,val)) ,@body))
 (defmacro let-n (syms vals &body body) `(let ,(mapcar #'list syms vals) ,@body))
 
+;; LOOP as it should be: an indefinite loop
+(defmacro loop (&body body)
+  (let-1 start (gensym)
+    `(block nil (tagbody ,start ,@body (go ,start)))))
+
+(defmacro loop-until (cond ret &body body)
+  `(loop (when ,cond (return ,ret)) ,@body))
+(defmacro loop-while (cond ret &body body)
+  `(loop (unless ,cond (return ,ret)) ,@body))
+
 ;; i like stacks :3
 (defparameter *stack* nil)
 (defun s (x) (setq *stack* (cons x *stack*)))
@@ -38,7 +65,7 @@
 ;; this allows you to write functions which take variables in the parent scope
 (defun ref (x) (cons x nil))
 (defun deref (ref) (car ref))
-(defun setref (ref x) (setf (car ref) x))
+(defun setref (ref x) (rplaca ref x))
 
 (defun read-input () (read-from-string (read-line)))
 (defun get-var (ref prompt)
@@ -54,8 +81,13 @@
 ;; should be a struct or something but i'm a cons addict and never
 ;; bothered to learn how to make data structures with any other tools
 (defun point (x y) (cons x y))
+(defun dup-point (p) (cons (car p) (cdr p)))
 (defun point-x (p) (car p))
 (defun point-y (p) (cdr p))
+(defun point-setx (p x) (rplaca p x))
+(defun point-sety (p y) (rplacd p y))
+(defun point-+ (p o)
+  (point (+ (point-x p) (point-x o)) (+ (point-y p) (point-y o))))
 
 (defun board-create (width height)
   (duplist (calltimes height (const (calltimes width (const nil))))))
@@ -66,10 +98,39 @@
   (let ((board-map
          (lambda (x)
            (match x eq " " (0 "X") (1 "O") (2 "#") (3 "*")))))
-    (format nil "~{~{ ~a │~}~%~^~:*~{───~^┼~*~}──~%~}"
+    (format nil "~{~{ ~a ~^│~}~%~^~:*~{~*───~^┼~}~%~}"
             (mapcar^2 board-map board))))
 
-;; get board info
+(defun <x< (min x max) (and (> x min) (< x max)))
+
+(defun board-check-at-point (board width height point win-len)
+  (let-1 piece (board-get board point)
+    (let-1 l
+        (split-list
+         (mapcar
+          (lambda (offset)
+            (let ((p (dup-point point))
+                  (tr (lambda (pt) (point-+ pt offset)))
+                  (c 0))
+              (loop-until
+               (or (not (and (<x< -1 (point-x p) width)
+                             (<x< -1 (point-y p) height)))
+                   (not (eq (board-get board p) piece))
+                   (= c win-len))
+               c
+               (setq p (f tr p))
+               (setq c (+ 1 c)))))
+          (list (point -1 0) (point 1 0)
+                (point 0 -1) (point 0 1)
+                (point -1 -1) (point 1 1)
+                (point 1 -1) (point -1 1))))
+    (when (car
+           (remove-if
+            #'null
+            (mapcar (lambda (a b) (> (+ a b) win-len)) (car l) (cdr l))))
+      piece))))
+
+;; user-facing stuff
 
 (defun get-board-info ()
   (eval ; this is not production code
