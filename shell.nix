bb67253583e77cadee841410cde7a70314f6bcae
Morgan Hager <goose121@users.noreply.github.com>
2025-06-27 17:38:54 -0600

    Add shell.nix for nix-shell based development

---
+++ shell.nix
@@ -0,0 +1,15 @@
+{ pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    # nativeBuildInputs is usually what you want -- tools you need to run
+    nativeBuildInputs = with pkgs.buildPackages; [
+      xorg.libX11.dev
+      xorg.libXcursor.dev
+      xorg.libXrandr.dev
+      xorg.libXinerama.dev
+      xorg.libXi.dev
+
+      libGL
+
+      sbcl
+    ];
+}
